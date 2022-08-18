78441a2f7e1147dcd12e41313f1e4677973b7537
Inae Lee <inae9709@hanmail.net>
2022-08-18 17:34:02 +0900

    init: project init

---
+++ TCA-memo/TCA-memo/Sources/MemoListViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ViewController.swift
+//  TCA-memo
+//
+//  Created by Devsisters on 2022/08/18.
+//
+
+import UIKit
+
+class MemoListViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = .white
+    }
+}
---
+++ TCA-memo/TCA-memoUITests/TCA_memoUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  TCA_memoUITestsLaunchTests.swift
+//  TCA-memoUITests
+//
+//  Created by Devsisters on 2022/08/18.
+//
+
+import XCTest
+
+class TCA_memoUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
