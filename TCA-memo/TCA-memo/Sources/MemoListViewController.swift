91fc07dc3e00b0a7268dcecc2b2a55ae3dd7a638
Inae Lee <inae9709@hanmail.net>
2022-08-19 12:03:55 +0900

    feat: memo list UI

--- TCA-memo/TCA-memo/Sources/MemoListViewController.swift
+++ TCA-memo/TCA-memo/Sources/MemoListViewController.swift
@@ -7,10 +7,46 @@
 
 import UIKit
 
-class MemoListViewController: UIViewController {
+class MemoListViewController: UITableViewController {
+    // MARK: - UI Components
+
+    lazy var addButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(didTapAddButton(_:)))
+
+    // MARK: - Life Cycle
+
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        setupProperty()
+    }
+
+    func setupProperty() {
+        title = "Memo"
         view.backgroundColor = .white
+
+        navigationController?.navigationBar.prefersLargeTitles = true
+        navigationItem.rightBarButtonItem = addButtonItem
+    }
+
+    func setupLayout() {}
+
+    // MARK: - Actions
+
+    @objc
+    func didTapAddButton(_ sender: UIBarButtonItem) {
+        print("add button")
+    }
+}
+
+extension MemoListViewController {
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        cell.accessoryType = .checkmark
+        
+        return cell
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
     }
 }
